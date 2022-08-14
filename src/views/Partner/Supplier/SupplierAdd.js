1d20b3f6d2e7c4e7c63d0e826711f19dba388624
congtran8991 <congtran8991@gmail.com>
2022-08-14 18:10:04 +0700

    update

--- src/views/Partner/Supplier/SupplierAdd.js
+++ src/views/Partner/Supplier/SupplierAdd.js
@@ -374,7 +374,7 @@ const SupplierAdd = ({ onRefresh }) => {
               </Grid>
               <Grid item xs={6} sm={4}>
                 <Dictionary
-                  diectionName="bank_cd"
+                  directionName="bank_cd"
                   onSelect={handleSelectBank}
                   label={t("partner.supplier.bank_cd")}
                   style={{ marginTop: 8, marginBottom: 4, width: "100%" }}
